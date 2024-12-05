6724b7179fc4b7fab95f892af65cd746db2142bf
Tim Hoiberg <github@hoiberg.dev>
2024-12-05 23:23:26 +0900

    Add part two solution
    Some of my worst work to date!

--- day_04/src/main.rs
+++ day_04/src/main.rs
@@ -1,9 +1,14 @@
+use std::collections::HashMap;
+
 fn main() {
     let input = include_str!("../data/puzzle_input.txt");
     let data = process_input(input);
 
     let part_one_answer = part_one_solution(&data);
     println!("The Part One answer is {part_one_answer}");
+
+    let part_two_answer = part_two_solution(&data);
+    println!("The Part Two answer is {part_two_answer}");
 }
 
 const DIRECTIONS: [(i8, i8); 8] = [
@@ -21,6 +26,50 @@ fn part_one_solution(data: &[Vec<char>]) -> usize {
     let xmas_words = find_xmas_substrings(data);
 
     xmas_words.len()
+}
+
+fn part_two_solution(data: &[Vec<char>]) -> u32 {
+    // TODO: reuse searching code, but only look for MAS
+    // then I think the rest of the general idea is correct-ish
+    let xmas_words = find_mas_substrings(data);
+
+    // iterate through and find all values that have the same A coord
+    // check that the corners are present in the word (doesn't matter what is where)
+    let mut intersecting_words: HashMap<(usize, usize), Vec<Vec<(usize, usize)>>> = HashMap::new();
+
+    for xmas in xmas_words {
+        if let Some(a) = intersecting_words.get_mut(&xmas[1]) {
+            a.push(xmas);
+        } else {
+            intersecting_words.insert(xmas[1], vec![xmas]);
+        }
+    }
+
+    let mut xdashmas_count = 0;
+
+    intersecting_words
+        .iter()
+        .for_each(|((a_x, a_y), intersections)| {
+            if a_x > &0 && a_y > &0 {
+                let top_left = (a_x - 1, a_y - 1); // -1,-1
+                let top_right = (a_x + 1, a_y - 1); // 1, -1
+                let bottom_left = (a_x - 1, a_y + 1); // -1, 1
+                let bottom_right = (a_x + 1, a_y + 1); // 1, 1
+
+                let does_intersect = [top_left, top_right, bottom_right, bottom_left]
+                    .iter()
+                    .all(|corner| intersections.iter().flatten().any(|pos| pos == corner));
+
+                if does_intersect {
+                    xdashmas_count += 1;
+                }
+            }
+            // for a
+            // get the corners and check if there is something present in all the corners
+            //   if so then add 1
+        });
+
+    xdashmas_count
 }
 
 fn find_xmas_substrings(grid: &[Vec<char>]) -> Vec<Vec<(usize, usize)>> {
@@ -52,6 +101,39 @@ fn find_xmas_substrings(grid: &[Vec<char>]) -> Vec<Vec<(usize, usize)>> {
                                             ]);
                                         }
                                     }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    matching_substr
+}
+
+fn find_mas_substrings(grid: &[Vec<char>]) -> Vec<Vec<(usize, usize)>> {
+    let mut matching_substr: Vec<Vec<(usize, usize)>> = vec![];
+
+    for (y, row) in grid.iter().enumerate() {
+        for (x, letter) in row.iter().enumerate() {
+            if letter == &'M' {
+                for (x_velocity, y_velocity) in DIRECTIONS {
+                    let possible_a = next_char(grid, (x, y), (x_velocity, y_velocity));
+
+                    if let Some((possible_a, a_coordinates)) = possible_a {
+                        if possible_a == &'A' {
+                            let possible_s =
+                                next_char(grid, a_coordinates, (x_velocity, y_velocity));
+
+                            if let Some((possible_s, s_coordinates)) = possible_s {
+                                if possible_s == &'S' {
+                                    matching_substr.push(vec![
+                                        (x, y),
+                                        a_coordinates,
+                                        s_coordinates,
+                                    ]);
                                 }
                             }
                         }
@@ -114,5 +196,19 @@ mod test_super {
         let puzzle_data = process_input(include_str!("../data/puzzle_input.txt"));
 
         assert_eq!(part_one_solution(&puzzle_data), 2562);
+    }
+
+    #[test]
+    fn test_part_two_example() {
+        let test_data = process_input(include_str!("../data/test_input.txt"));
+
+        assert_eq!(part_two_solution(&test_data), 9);
+    }
+
+    #[test]
+    fn test_part_two_solution() {
+        let puzzle_data = process_input(include_str!("../data/puzzle_input.txt"));
+
+        assert_eq!(part_two_solution(&puzzle_data), 1902);
     }
 }
