2dfb6db3c06452a6b68d4c6ec90ebf2d17832cfe
Tim Hoiberg <github@hoiberg.dev>
2024-12-06 18:02:00 +0900

    Refactor in preparation of part two

--- day_05/src/main.rs
+++ day_05/src/main.rs
@@ -3,37 +3,40 @@ use std::collections::{HashMap, HashSet};
 fn main() {
     let data = process_input(include_str!("../data/puzzle_input.txt"));
 
-    let part_one_answer = part_one_solution(&data);
+    let (ordered, _) = partition_ordered(data);
+
+    let part_one_answer = part_one_solution(&ordered);
     println!("The Part One answer is {part_one_answer}");
 }
 
 type PrintQueue = (HashMap<u32, Vec<u32>>, Vec<Vec<u32>>);
 
-fn part_one_solution((ordering_rules, updates): &PrintQueue) -> u32 {
-    updates
+fn part_one_solution(ordered_updates: &[Vec<u32>]) -> u32 {
+    ordered_updates
         .iter()
-        .map(|update| {
-            let mut expected_pages: HashSet<u32> = HashSet::new();
-
-            update.iter().for_each(|page| {
-                expected_pages.remove(page);
-                if let Some(dependencies) = ordering_rules.get(page) {
-                    dependencies.iter().for_each(|dep| {
-                        // TODO: look at better way to check
-                        if update.contains(dep) {
-                            let _ = expected_pages.insert(*dep);
-                        }
-                    });
-                }
-            });
-
-            if expected_pages.is_empty() {
-                update[update.len() / 2]
-            } else {
-                0
-            }
-        })
+        .map(|update| update[update.len() / 2])
         .sum()
+}
+
+fn partition_ordered((ordering_rules, updates): PrintQueue) -> (Vec<Vec<u32>>, Vec<Vec<u32>>) {
+    updates.into_iter().partition(|update| {
+        let mut expected_pages: HashSet<u32> = HashSet::new();
+
+        update.iter().for_each(|page| {
+            expected_pages.remove(page);
+
+            if let Some(dependencies) = ordering_rules.get(page) {
+                dependencies.iter().for_each(|dep| {
+                    // TODO: look at better way to check
+                    if update.contains(dep) {
+                        let _ = expected_pages.insert(*dep);
+                    }
+                });
+            }
+        });
+
+        expected_pages.is_empty()
+    })
 }
 
 fn process_input(input: &str) -> PrintQueue {
@@ -85,15 +88,16 @@ mod test_super {
 
     #[test]
     fn test_part_one_example() {
-        let test_data = test_data();
+        let (ordered, _) = partition_ordered(test_data());
 
-        assert_eq!(part_one_solution(&test_data), 143);
+        assert_eq!(part_one_solution(&ordered), 143);
     }
 
     #[test]
     fn test_part_one_solution() {
-        let data = process_input(include_str!("../data/puzzle_input.txt"));
+        let (ordered, _) =
+            partition_ordered(process_input(include_str!("../data/puzzle_input.txt")));
 
-        assert_eq!(part_one_solution(&data), 5713);
+        assert_eq!(part_one_solution(&ordered), 5713);
     }
 }
