5e7f60e9565694c105fa427c8893c3b9d0d9fe7e
Tim Hoiberg <github@hoiberg.dev>
2024-12-06 23:46:42 +0900

    Add solution for Part 2

--- day_05/src/main.rs
+++ day_05/src/main.rs
@@ -1,12 +1,15 @@
 use std::collections::{HashMap, HashSet};
 
 fn main() {
-    let data = process_input(include_str!("../data/puzzle_input.txt"));
+    let (ordering_rules, updates) = process_input(include_str!("../data/puzzle_input.txt"));
 
-    let (ordered, _) = partition_ordered(data);
+    let (ordered, unordered) = partition_ordered(&ordering_rules, updates);
 
     let part_one_answer = part_one_solution(&ordered);
     println!("The Part One answer is {part_one_answer}");
+
+    let part_two_answer = part_two_solution(&ordering_rules, &unordered);
+    println!("The Part Two answer is {part_two_answer}");
 }
 
 type PrintQueue = (HashMap<u32, Vec<u32>>, Vec<Vec<u32>>);
@@ -18,7 +21,43 @@ fn part_one_solution(ordered_updates: &[Vec<u32>]) -> u32 {
         .sum()
 }
 
-fn partition_ordered((ordering_rules, updates): PrintQueue) -> (Vec<Vec<u32>>, Vec<Vec<u32>>) {
+fn part_two_solution(
+    ordering_rules: &HashMap<u32, Vec<u32>>,
+    unordered_updates: &[Vec<u32>],
+) -> u32 {
+    unordered_updates
+        .iter()
+        .map(|update| {
+            let mut ordered: Vec<u32> = vec![];
+
+            update.iter().for_each(|page| {
+                let page_deps = ordering_rules.get(page);
+
+                if ordered.is_empty() || page_deps.is_none() {
+                    ordered.push(*page);
+                } else {
+                    for i in 0..ordered.len() {
+                        if page_deps.unwrap().contains(&ordered[i]) {
+                            ordered.insert(i, *page);
+                            break;
+                        }
+
+                        if i == (ordered.len() - 1) {
+                            ordered.push(*page);
+                        }
+                    }
+                }
+            });
+
+            ordered[ordered.len() / 2]
+        })
+        .sum()
+}
+
+fn partition_ordered(
+    ordering_rules: &HashMap<u32, Vec<u32>>,
+    updates: Vec<Vec<u32>>,
+) -> (Vec<Vec<u32>>, Vec<Vec<u32>>) {
     updates.into_iter().partition(|update| {
         let mut expected_pages: HashSet<u32> = HashSet::new();
 
@@ -88,16 +127,33 @@ mod test_super {
 
     #[test]
     fn test_part_one_example() {
-        let (ordered, _) = partition_ordered(test_data());
+        let test_data = test_data();
+        let (ordered, _) = partition_ordered(&test_data.0, test_data.1);
 
         assert_eq!(part_one_solution(&ordered), 143);
     }
 
     #[test]
     fn test_part_one_solution() {
-        let (ordered, _) =
-            partition_ordered(process_input(include_str!("../data/puzzle_input.txt")));
+        let puzzle_data = process_input(include_str!("../data/puzzle_input.txt"));
+        let (ordered, _) = partition_ordered(&puzzle_data.0, puzzle_data.1);
 
         assert_eq!(part_one_solution(&ordered), 5713);
+    }
+
+    #[test]
+    fn test_part_two_example() {
+        let test_data = test_data();
+        let (_, unordered) = partition_ordered(&test_data.0, test_data.1);
+
+        assert_eq!(part_two_solution(&test_data.0, &unordered), 123);
+    }
+
+    #[test]
+    fn test_part_two_answer() {
+        let puzzle_data = process_input(include_str!("../data/puzzle_input.txt"));
+        let (_, unordered) = partition_ordered(&puzzle_data.0, puzzle_data.1);
+
+        assert_eq!(part_two_solution(&puzzle_data.0, &unordered), 5180);
     }
 }
