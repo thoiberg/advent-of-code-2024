9579621888c4e4924e435678b0c9e3691543158d
Tim Hoiberg <github@hoiberg.dev>
2024-12-08 22:57:33 +0900

    Add solution for Part 2

--- day_07/src/main.rs
+++ day_07/src/main.rs
@@ -3,6 +3,9 @@ fn main() {
 
     let part_one_answer = part_one_solution(&data);
     println!("The Part One answer is {part_one_answer}");
+
+    let part_two_answer = part_two_solution(&data);
+    println!("Then Part Two answer is {part_two_answer}");
 }
 
 type Calibration = (u64, Vec<u64>);
@@ -11,6 +14,20 @@ fn part_one_solution(data: &[Calibration]) -> u64 {
     data.iter()
         .filter_map(|(expected_total, values)| {
             let totals = calculate(*expected_total, values);
+
+            if totals.iter().any(|total| total == expected_total) {
+                Some(expected_total)
+            } else {
+                None
+            }
+        })
+        .sum()
+}
+
+fn part_two_solution(data: &[Calibration]) -> u64 {
+    data.iter()
+        .filter_map(|(expected_total, values)| {
+            let totals = calculate_part_two(*expected_total, values);
 
             if totals.iter().any(|total| total == expected_total) {
                 Some(expected_total)
@@ -53,6 +70,7 @@ fn run_through(limit: u64, total: u64, operation: Operation, remaining: &[u64]) 
     let new_total = match operation {
         Operation::Add => total + next_number,
         Operation::Multiply => total * next_number,
+        _ => panic!("oh no"),
     };
 
     if new_total > limit {
@@ -70,10 +88,55 @@ fn run_through(limit: u64, total: u64, operation: Operation, remaining: &[u64]) 
     }
 }
 
+fn calculate_part_two(limit: u64, values: &[u64]) -> Vec<u64> {
+    [
+        run_through_part_two(limit, values[0], Operation::Add, &values[1..]),
+        run_through_part_two(limit, values[0], Operation::Multiply, &values[1..]),
+        run_through_part_two(limit, values[0], Operation::Concatenation, &values[1..]),
+    ]
+    .concat()
+}
+
+fn run_through_part_two(
+    limit: u64,
+    total: u64,
+    operation: Operation,
+    remaining: &[u64],
+) -> Vec<u64> {
+    let next_number = remaining[0];
+
+    let new_total = match operation {
+        Operation::Add => total + next_number,
+        Operation::Multiply => total * next_number,
+        Operation::Concatenation => {
+            (total.to_string() + &next_number.to_string())
+                .parse::<u64>()
+                .unwrap()
+            // (total * (next_number.to_string().len() as u64)) + next_number
+        }
+    };
+
+    if new_total > limit {
+        return vec![];
+    }
+
+    if remaining.len() > 1 {
+        [
+            run_through_part_two(limit, new_total, Operation::Add, &remaining[1..]),
+            run_through_part_two(limit, new_total, Operation::Multiply, &remaining[1..]),
+            run_through_part_two(limit, new_total, Operation::Concatenation, &remaining[1..]),
+        ]
+        .concat()
+    } else {
+        vec![new_total]
+    }
+}
+
 #[derive(Debug)]
 enum Operation {
     Add,
     Multiply,
+    Concatenation,
 }
 
 #[cfg(test)]
@@ -96,6 +159,20 @@ mod test_super {
 
     #[test]
     fn test_calculate_returns_correct_totals() {
-        // assert_eq!(calculate(&[10, 19]), [29, 190]);
+        assert_eq!(calculate(190, &[10, 19]), [29, 190]);
+    }
+
+    #[test]
+    fn test_part_two_example() {
+        let data = process_input(include_str!("../data/test_input.txt"));
+
+        assert_eq!(part_two_solution(&data), 11_387);
+    }
+
+    #[test]
+    fn test_part_two_solution() {
+        let data = process_input(include_str!("../data/puzzle_input.txt"));
+
+        assert_eq!(part_two_solution(&data), 492_383_931_650_959);
     }
 }
