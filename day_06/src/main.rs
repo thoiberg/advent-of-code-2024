987920411956ebc6f4ad1bf3bd4307db175a7283
Tim Hoiberg <github@hoiberg.dev>
2024-12-08 18:39:35 +0900

    Failed attempt at part 2

--- day_06/src/main.rs
+++ day_06/src/main.rs
@@ -1,40 +1,60 @@
+// TODO for Part 2:
+//  1680 is too high
+//  need to simplify the logic, rather than constantly pivoting after moving
+//  I want to try only pivoting at the start of each move to see if that provides a more
+//  accurate answer. Doing that here resulted in a off by 1 error in all tests
+
 use std::collections::HashSet;
 
 use ndarray::Array2;
 
 fn main() {
     let data = process_input(include_str!("../data/puzzle_input.txt"));
+    let guard_path = map_path(&data);
 
-    let part_one_answer = part_one_solution(&data);
+    let part_one_answer = part_one_solution(&guard_path);
     println!("Part one answer is {part_one_answer}");
+
+    let part_two_answer = part_two_solution(&guard_path, &data);
+    println!("Part two answer is {part_two_answer}");
 }
 
-fn part_one_solution(grid: &Array2<char>) -> usize {
-    let mut guard = place_guard(grid);
+type GuardPath = Vec<((usize, usize), Direction)>;
 
-    let mut still_on_grid = true;
-    let mut visited = HashSet::new();
-    visited.insert(guard.position());
+fn part_one_solution(path: &GuardPath) -> usize {
+    let pos_only = path.iter().map(|(pos, _)| pos);
 
-    while still_on_grid {
-        guard.r#move();
+    let distinct_pos: HashSet<&(usize, usize)> = HashSet::from_iter(pos_only);
 
-        visited.insert(guard.position());
+    distinct_pos.len()
+}
 
-        let mut front = guard.front();
+fn part_two_solution(path: &GuardPath, grid: &Array2<char>) -> u32 {
+    // 1680 is too high
+    // could be a dupe thing? Might try cloning values to another array and skipping if I've already checked
+    let mut infinite_loop_count = 0;
 
-        while front
-            .and_then(|pos| grid.get(pos))
-            .is_some_and(|point| point == &'#')
-        {
-            guard.turn_right();
-            front = guard.front();
-        }
+    for (i, ((y, x), _)) in path.iter().enumerate().skip(1) {
+        let ((prev_y, prev_x), prev_direction) = path[i - 1]; // safe as skipping first element
+        let guard = Guard {
+            y: prev_y,
+            x: prev_x,
+            direction: prev_direction,
+        };
 
-        still_on_grid = front.and_then(|f| grid.get(f)).is_some();
+        // println!("block is at: {:?}", (y, x));
+        // println!("guard is at {:?}", guard.position());
+        // println!("-------------------------");
+
+        let mut grid_with_block = grid.clone();
+        grid_with_block[(*y, *x)] = '#';
+
+        if check_infinite_loop(&grid_with_block, guard) {
+            infinite_loop_count += 1;
+        };
     }
 
-    visited.len()
+    infinite_loop_count
 }
 
 fn process_input(input: &str) -> Array2<char> {
@@ -57,6 +77,67 @@ fn place_guard(grid: &Array2<char>) -> Guard {
         .unwrap()
 }
 
+fn check_infinite_loop(grid: &Array2<char>, mut guard: Guard) -> bool {
+    let mut still_on_grid = true;
+    let mut back_to_start = false;
+    let mut walked_places: HashSet<((usize, usize), Direction)> = HashSet::new();
+
+    while still_on_grid && !back_to_start {
+        let mut front = guard.front();
+
+        walked_places.insert(guard.position());
+
+        while front
+            .and_then(|pos| grid.get(pos))
+            .is_some_and(|point| point == &'#')
+        {
+            guard.turn_right();
+            front = guard.front();
+        }
+        guard.r#move();
+
+        if front.and_then(|f| grid.get(f)).is_some() {
+            let next = front.unwrap();
+
+            if walked_places.contains(&(next, guard.direction)) {
+                back_to_start = true;
+            }
+        } else {
+            still_on_grid = false;
+        }
+    }
+
+    back_to_start
+}
+
+fn map_path(grid: &Array2<char>) -> GuardPath {
+    let mut guard = place_guard(grid);
+
+    let mut still_on_grid = true;
+    let mut visited = vec![guard.position()];
+
+    while still_on_grid {
+        guard.r#move();
+
+        let mut front = guard.front();
+
+        while front
+            .and_then(|pos| grid.get(pos))
+            .is_some_and(|point| point == &'#')
+        {
+            guard.turn_right();
+            front = guard.front();
+        }
+
+        visited.push(guard.position());
+
+        still_on_grid = front.and_then(|f| grid.get(f)).is_some();
+    }
+
+    visited
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
 enum Direction {
     Top,
     Right,
@@ -80,8 +161,8 @@ impl Guard {
         }
     }
 
-    fn position(&self) -> (usize, usize) {
-        (self.y, self.x)
+    fn position(&self) -> ((usize, usize), Direction) {
+        ((self.y, self.x), self.direction)
     }
 
     fn front(&self) -> Option<(usize, usize)> {
@@ -118,14 +199,67 @@ mod test_super {
     #[test]
     fn test_part_one_example() {
         let grid = process_input(include_str!("../data/test_input.txt"));
+        let guard_path = map_path(&grid);
 
-        assert_eq!(part_one_solution(&grid), 41);
+        assert_eq!(part_one_solution(&guard_path), 41);
     }
 
     #[test]
     fn test_part_one_solution() {
         let grid = process_input(include_str!("../data/puzzle_input.txt"));
+        let guard_path = map_path(&grid);
 
-        assert_eq!(part_one_solution(&grid), 4433);
+        assert_eq!(part_one_solution(&guard_path), 4433);
     }
+
+    #[test]
+    fn test_part_two_example() {
+        let grid = process_input(include_str!("../data/test_input.txt"));
+        let guard_path = map_path(&grid);
+
+        assert_eq!(part_two_solution(&guard_path, &grid), 6);
+    }
+
+    // #[test]
+    // fn test_check_infinite_loop_ironically_does_not_get_stuck() {
+    //     let mut grid = process_input(include_str!("../data/puzzle_input.txt"));
+
+    //     //         block is at: (71, 71)
+    //     // guard is at: ((72, 71), Top)
+
+    //     grid[(71, 71)] = '#';
+
+    //     let guard = Guard {
+    //         x: 71,
+    //         y: 72,
+    //         direction: Direction::Top,
+    //     };
+
+    //     let outcome = check_infinite_loop(&grid, guard, false);
+
+    //     assert!(!outcome);
+    // }
+
+    // #[test]
+    // fn test_more_checks() {
+    //     let mut grid = process_input(include_str!("../data/puzzle_input.txt"));
+
+    //     //         block is at: (65, 117)
+    //     // guard is at ((66, 117), Top)
+
+    //     grid[(65, 117)] = '#';
+
+    //     println!("{:?}", grid.get((66, 117)));
+    //     println!("{:?}", grid.get((66, 118)));
+
+    //     let guard = Guard {
+    //         y: 66,
+    //         x: 117,
+    //         direction: Direction::Top,
+    //     };
+
+    //     let outcome = check_infinite_loop(&grid, guard, false);
+
+    //     assert!(!outcome);
+    // }
 }
